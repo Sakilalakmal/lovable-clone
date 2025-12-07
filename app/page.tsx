8995ebce39240bc5c250c8bde369982701bd5d4b
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-07 10:51:35 +0530

    feat(trpc): implement TRPC setup with context, routers, and client integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { useTRPC } from "./trpc/client";
+import { json } from "zod";
+import { caller } from "./trpc/server";
+
 export default async function Home() {
+  const data = await caller.hello({ text: "Lovable" });
+
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16"></div>
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      {JSON.stringify(data)}
+    </div>
   );
 }
